5b3e718268fc1b849f5062aa369bf8eaa2dc8da5
raymond1242 <raymond-455@hotmail.com>
2024-04-19 11:42:22 -0500

    feat: adding bid creation request and update api.yaml

---
+++ core/serializers/bid.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from core.models import Bid
+
+
+class BidSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bid
+        fields = "__all__"
+
+
+class BidCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bid
+        fields = [
+            "amount",
+            "auction",
+        ]
+        extra_kwargs = {
+            "amount": {"required": True},
+            "auction": {"required": True},
+        }
+
+    def validate(self, data):
+        if data["price"] < data["auction"].price:
+            raise serializers.ValidationError(
+                "Price must be higher than the current price"
+            )
+        return data
---
+++ core/views/bid.py
@@ -0,0 +1,41 @@
+from rest_framework import status
+from rest_framework import mixins, viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from drf_yasg.utils import swagger_auto_schema
+
+from core.serializers.bid import BidSerializer, BidCreateSerializer
+
+from core.models import Bid, Customer
+
+
+class BidViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = Bid.objects.all()
+    serializer_class = BidCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Bid.objects.filter(customer=self.request.user.customer)
+
+    @swagger_auto_schema(
+        request_body=BidCreateSerializer,
+        responses={status.HTTP_201_CREATED: BidSerializer},
+    )
+    def create(self, request, *args, **kwargs):
+        customer = request.user.customer
+
+        if customer.type != Customer.BUYER:
+            return Response(
+                {"error": "Only buyers can create bids"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(customer=customer)
+
+        auction = serializer.validated_data["auction"]
+        auction.price = serializer.validated_data["amount"]
+        auction.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

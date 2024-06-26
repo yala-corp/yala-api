c5cc4225d912f2f7539180958ae727c546825b05
raymond1242 <raymond-455@hotmail.com>
2024-06-25 21:56:26 -0500

    feat: remove unused import

--- core/views/auction.py
+++ core/views/auction.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.decorators import action
 from drf_yasg.utils import swagger_auto_schema
 
 from core.serializers.auction import AuctionSerializer, AuctionCreateSerializer

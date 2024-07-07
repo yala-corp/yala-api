824df9162039ce0768a76c895cfb048072c6f944
raymond1242 <raymond-455@hotmail.com>
2024-07-06 23:46:31 -0500

    feat: fix media storage issue

--- config/settings.py
+++ config/settings.py
@@ -166,7 +166,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 
-
+STATIC_ROOT = "static/"
 STATICFILES_LOCATION = "static"
 MEDIAFILES_LOCATION = "media"
 
@@ -190,5 +190,3 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
